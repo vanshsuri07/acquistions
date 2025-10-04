9e011fabc80b2fd02de2b734d4031083ed681bf7
Vanshsuri07 <vanshsuri07@gmai.com>
2025-10-04 15:13:04 +0530

    Implement eslint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from Acquistions!");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from Acquistions!');
 });
 
 export default app;
